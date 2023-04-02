8f9f61e6cc02ac62cc6f7b0e0c62b05732563669
gyustur0409 <wnsgud0312@gmail.com>
2023-04-02 16:30:57 +0900

    roadmove add
    도로움직이는거 추가함

---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    public GameObject[] road;
+    public GameObject[] runningRoad;
+    public int roadNum, runningRoadNum;
+    void Start()
+    {
+        runningRoadNum=0;
+        runningRoad[runningRoadNum] = Instantiate(road[0],new Vector3(0,0,0),transform.rotation);
+        Destroy(runningRoad[runningRoadNum],3f);
+        runningRoadNum++;
+        runningRoad[runningRoadNum] = Instantiate(road[Random.Range(0,10)],new Vector3(0,0,30),transform.rotation);
+        Destroy(runningRoad[runningRoadNum],6f);
+        runningRoadNum++;
+        runningRoad[runningRoadNum] = Instantiate(road[Random.Range(0,10)],new Vector3(0,0,60),transform.rotation);
+        Destroy(runningRoad[runningRoadNum],9f);
+        runningRoadNum =0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(runningRoad[runningRoadNum]==null){
+            runningRoad[runningRoadNum] = Instantiate(road[Random.Range(0,10)],new Vector3(0,0,59.7f),transform.rotation);
+            Destroy(runningRoad[runningRoadNum],9f);
+            if(runningRoadNum==2){
+                runningRoadNum=0;
+            }
+            else{
+                runningRoadNum++;
+            }
+        }
+    }
+}

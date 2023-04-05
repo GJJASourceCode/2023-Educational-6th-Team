6b94c95dec6ccaa79be3841efd725dd93a333dfc
caeeunhg <ms78060722@gmail.com>
2023-04-05 16:38:47 +0900

    f

--- Assets/Scripts/Ob.cs
+++ Assets/Scripts/Ob.cs
@@ -7,7 +7,11 @@ public class Ob : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        void OnCollisionEnter(Collision Collision){
+            if(Collision.collider.gameObject.CompareTag("Player")){
+                Debug.Log("플레이어접촉");
+            }
+        }
     }
 
     // Update is called once per frame
